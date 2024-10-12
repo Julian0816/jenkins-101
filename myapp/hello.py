9f8437b95eb0cceeb666d946073b75b0d43fc201
Julian <julian.martinez.cs@gmail.com>
2024-10-12 21:30:33 +0000

    testing vim

--- myapp/hello.py
+++ myapp/hello.py
@@ -1,7 +1,7 @@
 import fire
 
 def hello(name="World"):
-  return "Hello %s! I hope everything is great" % name
+  return "Hello %s! I hope everything is grea, and now we are using vim to edit this filet" % name
 
 
 if __name__ == '__main__':
